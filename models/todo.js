2473846a074ae6bd6e84daee90586bbd6d9bf4cc
ray1025t <a7807230@gmail.com>
2021-10-09 11:27:39 +0800

    feat: revise mongoose

--- models/todo.js
+++ models/todo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const todoSchema = new Schema({
-  name:{
-    type: string, // 資料型別字串
-    required: true // 必填欄位
+  name: {
+    type: String, //  型別為字串
+    required: true // 必需的欄位
   }
 })
 
