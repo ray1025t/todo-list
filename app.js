bf595bfa395758fa17ae8ff777c188535f8692ed
ray1025t <a7807230@gmail.com>
2021-10-10 09:46:01 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -77,6 +77,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.error(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
